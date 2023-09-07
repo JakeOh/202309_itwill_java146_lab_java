28b890422db0ef0261a8cd104fa196902ad54350
JakeOh <jake.oh.lecture@gmail.com>
2023-09-07 18:06:25 +0900

    variable

---
+++ lab02_variable/src/com/itwill/variable02/VariableMain02.java
@@ -0,0 +1,14 @@
+package com.itwill.variable02;
+
+public class VariableMain02 {
+
+    public static void main(String[] args) {
+        // 정수를 저장하는 변수(x)를 선언하고, 값을 저장(초기화)
+        
+        // 정수를 저장하는 변수(y)를 선언하고, 값을 저장(초기화)
+        
+        // 두 변수 x와 y의 사칙연산(+, -, *, /) 결과를 출력.
+
+    }
+
+}
