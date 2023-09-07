a86a197625cdea1d7299e3a9debcb766b62e82e3
Jake Oh <jake.oh.lecture@gmail.com>
2023-09-07 15:29:46 +0900

    initial commit

---
+++ lab01_intro/src/com/itwill/intro/AppMain.java
@@ -0,0 +1,20 @@
+package com.itwill.intro; // 패키지 선언문
+
+// inline 주석(comment): //로 시작, 그 줄의 끝까지.
+
+/*
+ * block 주석(comment)
+ * 문서화 주석(documentation comment) - 클래스 설명, 메서드 설명.
+ */
+
+public class AppMain {  // 클래스 선언 시작.
+
+    // main 메서드(method): 자바 프로그램 시작.
+    public static void main(String[] args) {
+        // 주석(comment): 프로그램의 설명.
+        System.out.println("안녕하세요.");
+        System.out.println("Hello, Java!");
+    }
+    // main 메서드가 끝나면 자바 프로그램이 종료. 
+
+} // 클래스 선언 끝.
