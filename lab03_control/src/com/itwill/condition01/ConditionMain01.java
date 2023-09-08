4e074daa8fe836b89d31a01535d2f4bb4213007f
JakeOh <jake.oh.lecture@gmail.com>
2023-09-08 15:22:56 +0900

    conditional statement

---
+++ lab03_control/src/com/itwill/condition01/ConditionMain01.java
@@ -0,0 +1,17 @@
+package com.itwill.condition01;
+
+public class ConditionMain01 {
+
+    public static void main(String[] args) {
+        // 조건문(conditional statement)
+        
+        int number = -123;
+        if (number > 0) {
+            // 조건식을 만족하는 경우에 실행할 코드 블록
+            System.out.println("양수");
+        }
+
+        System.out.println("=== 프로그램 끝 ===");
+    }
+
+}
