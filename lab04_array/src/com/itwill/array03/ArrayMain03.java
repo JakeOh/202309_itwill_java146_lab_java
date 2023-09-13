adff3ce4431e5e9aa07a1356c7533bf280d4529f
JakeOh <jake.oh.lecture@gmail.com>
2023-09-13 17:23:44 +0900

    array

---
+++ lab04_array/src/com/itwill/array03/ArrayMain03.java
@@ -0,0 +1,35 @@
+package com.itwill.array03;
+
+import java.util.Random;
+
+public class ArrayMain03 {
+
+    public static void main(String[] args) {
+        // 향상된 for 구문은 배열에 저장된 값들을 순서대로 읽는 경우에만 사용.
+        // (주의) 향상된 for 구문은 배열의 원소를 저장/변경할 때 이용할 수 없음.
+        // 배열의 원소를 저장/변경할 때는 반드시 "배열[인덱스] = 값;" 문장을 사용해야만 함. 
+        
+        Random random = new Random();
+        
+        int[] numbers = new int[3]; // 정수(int) 3개를 저장할 수 있는 배열.
+        for (int x : numbers) {
+            x = 10; // numbers의 원소가 변경되는 것이 아니라, 지역변수 x의 값만 변경.
+        }
+        
+        for (int x : numbers) {
+            System.out.print(x + " ");  //-> 0을 출력
+        }
+        System.out.println();
+        
+        // Ex. 
+        // (1) 정수(int) 10개를 저장할 수 있는 배열을 만들고,
+        // (2) 1 이상 4 이하(5 미만)의 정수들을 임의로 저장하고 
+        // (3) 그 내용을 출력.
+        // (4) 문자열 10개를 저장할 수 있는 배열을 만들고,
+        // (5) (1)에서 만들 배열의 원소가 1 또는 3이면 "남성"을 저장, 2 또는 4이면 "여성"을 저장.
+        // (6) 문자열 배열 내용을 출력.
+        
+
+    }
+
+}
