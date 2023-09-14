743da95b2395e5edde6d290bdee5c4e4d0fb802a
JakeOh <jake.oh.lecture@gmail.com>
2023-09-14 17:24:26 +0900

    exercise

--- lab05_method/src/com/itwill/method05/MethodMain05.java
+++ lab05_method/src/com/itwill/method05/MethodMain05.java
@@ -8,16 +8,16 @@ public class MethodMain05 {
         int[] scores = {70, 60, 100, 50, 81};
         
         int sum = sum(scores);
-        System.out.println("총점 = " + sum); // 361
+        System.out.println("총점 = " + sum);
         
         double avg = mean(scores);
-        System.out.println("평균 = " + avg); // 72.2
+        System.out.println("평균 = " + avg);
         
         int max = max(scores);
-        System.out.println("최댓값 = " + max); // 100
+        System.out.println("최댓값 = " + max);
         
         int min = min(scores);
-        System.out.println("최솟값 = " + min); // 50
+        System.out.println("최솟값 = " + min);
 
     }
 
