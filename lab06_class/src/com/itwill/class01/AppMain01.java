4471b6004573f00ecee371b9de9b4c128935a5d6
JakeOh <jake.oh.lecture@gmail.com>
2023-09-15 18:21:26 +0900

    oop, constructor

--- lab06_class/src/com/itwill/class01/AppMain01.java
+++ lab06_class/src/com/itwill/class01/AppMain01.java
@@ -4,8 +4,8 @@ public class AppMain01 {
 
     public static void main(String[] args) {
         // 클래스: 데이터 타입.
-        // 생성자(constructor): 클래스에 선언된 변수(필드)들을 초기화.
-        Subject subject = new Subject();
+        // 생성자(constructor): 클래스에 선언된 변수(필드)들을 초기화(initialize).
+        Subject subject = new Subject(); // 생성자 호출. 객체 생성. 인스턴스화.
         
         System.out.println(subject.java);
         System.out.println(subject.sql);
@@ -14,8 +14,13 @@ public class AppMain01 {
         subject.java = 100;
         subject.sql = 100;
         subject.js = 50;
-        
+        System.out.println("java = " + subject.java);
 
+        Subject sub2 = new Subject(100, 50, 10);
+        System.out.println(sub2.java);
+        System.out.println(sub2.sql);
+        System.out.println(sub2.js);
+        
     }
 
 }
--- lab06_class/src/com/itwill/class01/Subject.java
+++ lab06_class/src/com/itwill/class01/Subject.java
@@ -6,5 +6,27 @@ public class Subject {
     int java;
     int sql;
     int js;
+    
+    // 생성자(constructor)
+    // 객체를 생성하고, 객체의 필드들을 초기화하는 역할.
+    // 생성자의 이름은 반드시 클래스의 이름과 같아야 함.
+    // 메서드를 작성하는 방법과 비슷. 리턴 타입이 없음!
+
+    // 기본 생성자(default constructor):
+    // 아규먼트를 갖지 않는 생성자.
+    // 일반적으로 (특별한 코드가 없어도) 필드 타입의 기본값으로 필드들을 초기화.
+    // - 정수 기본값: 0, 실수 기본값: 0.0, boolean 기본값: false, 참조타입: null.
+    public Subject() {
+        // 아무 코드를 작성하지 않아도 됨.
+        // 하지만 블록 괄호({})는 생략할 수 없음.
+    }
+    
+    
+    // 아규먼트를 갖는 생성자 - 아규먼트의 값들로 필드들을 초기화.
+    public Subject(int java, int sql, int js) {
+        this.java = java;
+        this.sql = sql;
+        this.js = js;
+    }
 
 }
