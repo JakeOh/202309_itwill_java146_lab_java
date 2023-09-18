78e53801cc5f4c101d06128a795d593beaebc5bb
JakeOh <jake.oh.lecture@gmail.com>
2023-09-18 11:23:59 +0900

    oop, field, constructor, method

--- lab06_class/src/com/itwill/class01/AppMain01.java
+++ lab06_class/src/com/itwill/class01/AppMain01.java
@@ -5,22 +5,38 @@ public class AppMain01 {
     public static void main(String[] args) {
         // 클래스: 데이터 타입.
         // 생성자(constructor): 클래스에 선언된 변수(필드)들을 초기화(initialize).
-        Subject subject = new Subject(); // 생성자 호출. 객체 생성. 인스턴스화.
+        Subject sub1 = new Subject(); // 생성자 호출. 객체 생성. 인스턴스화.
         
-        System.out.println(subject.java);
-        System.out.println(subject.sql);
-        System.out.println(subject.js);
+        System.out.println(sub1.java);
+        System.out.println(sub1.sql);
+        System.out.println(sub1.js);
         
-        subject.java = 100;
-        subject.sql = 100;
-        subject.js = 50;
-        System.out.println("java = " + subject.java);
+        sub1.java = 100;
+        sub1.sql = 100;
+        sub1.js = 50;
+        System.out.println("java = " + sub1.java);
 
         Subject sub2 = new Subject(100, 50, 10);
         System.out.println(sub2.java);
         System.out.println(sub2.sql);
         System.out.println(sub2.js);
+     
+        // 함수형 프로그래밍(functional programming)
+        int total = getTotal(sub1);
+        System.out.println("sub1 total = " + total);
+        System.out.println("sub2 total = " + getTotal(sub2));
         
+        // 객체지향 프로그래밍(object-oriented programming)
+        System.out.println("sub1 total = " + sub1.getTotal());
+        System.out.println("sub1 mean = " + sub1.getMean());
+        
+        System.out.println("sub2 total = " + sub2.getTotal());
+        System.out.println("sub2 mean = " + sub2.getMean());
     }
 
+    // Subject 타입의 객체를 아규먼트로 전달받아서 세 과목의 총점을 반환하는 기능.
+    public static int getTotal(Subject subject) {
+        return subject.java + subject.sql + subject.js;
+    }
+    
 }
--- lab06_class/src/com/itwill/class01/Subject.java
+++ lab06_class/src/com/itwill/class01/Subject.java
@@ -1,5 +1,6 @@
 package com.itwill.class01;
 
+// 클래스: 데이터(필드) + 초기화(생성자) + 기능(메서드) => 데이터 타입
 public class Subject {
     
     // 필드(field). 멤버 변수.
@@ -11,7 +12,12 @@ public class Subject {
     // 객체를 생성하고, 객체의 필드들을 초기화하는 역할.
     // 생성자의 이름은 반드시 클래스의 이름과 같아야 함.
     // 메서드를 작성하는 방법과 비슷. 리턴 타입이 없음!
-
+    // 클래스를 작성할 때 생성자를 하나도 정의하지 않으면,
+    // 자바 컴파일러가 "기본 생성자"를 자동으로 만들어 줌.
+    // 클래스를 작성할 때 생성자를 직접 작성하면, 
+    // 자바 컴파일러는 기본 생성자를 자동으로 만들지 않음!
+    // 생성자 오버로딩(overloading): 생성자를 여러개 정의하는 것.
+    
     // 기본 생성자(default constructor):
     // 아규먼트를 갖지 않는 생성자.
     // 일반적으로 (특별한 코드가 없어도) 필드 타입의 기본값으로 필드들을 초기화.
@@ -21,12 +27,20 @@ public class Subject {
         // 하지만 블록 괄호({})는 생략할 수 없음.
     }
     
-    
     // 아규먼트를 갖는 생성자 - 아규먼트의 값들로 필드들을 초기화.
     public Subject(int java, int sql, int js) {
         this.java = java;
         this.sql = sql;
         this.js = js;
     }
+    
+    // 메서드(기능)
+    public int getTotal() {
+        return this.java + this.sql + this.js;
+    }
+    
+    public double getMean() {
+        return (double) this.getTotal() / 3;
+    }
 
-}
+} // end class Subject
