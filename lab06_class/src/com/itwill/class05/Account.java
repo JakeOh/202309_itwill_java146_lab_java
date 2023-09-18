88913a46c7660c84924ed355eb240ed0306dfdfa
JakeOh <jake.oh.lecture@gmail.com>
2023-09-18 17:29:30 +0900

    class exercise

---
+++ lab06_class/src/com/itwill/class05/Account.java
@@ -0,0 +1,41 @@
+package com.itwill.class05;
+
+/**
+ * 은행 계좌 정보.
+ * 속성: 계좌번호(int), 잔고(int)
+ * 기능: 입금, 출금, 이체, 정보 출력
+ */
+public class Account {
+    // field
+    
+    // constructor - argument 2개를 갖는 생성자.
+    
+    // method
+    /**
+     * deposit(입금).
+     * 
+     * @param amount 입금액.
+     * @return 입금 후 잔액.
+     */
+    
+    /**
+     * withdraw(출금).
+     * 
+     * @param amount 출금액.
+     * @return 출금 후 잔액.
+     */
+    
+    /**
+     * transfer(이체).
+     * 
+     * @param to 이체할 계좌(Account 타입).
+     * @param amount 이체할 금액.
+     * @return true. 
+     */
+    
+    /**
+     * info(정보).
+     * 계좌 번호, 현재 잔액을 출력.
+     */
+
+}
---
+++ lab06_class/src/com/itwill/class05/AppMain05.java
@@ -0,0 +1,10 @@
+package com.itwill.class05;
+
+public class AppMain05 {
+
+    public static void main(String[] args) {
+        // TODO: Account 타입 객체 생성를 생성하고 메서드들을 테스트.
+
+    }
+
+}
