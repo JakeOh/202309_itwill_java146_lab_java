c1d10a45a225b51ed6547a0b900933ecdeec61f8
JakeOh <jake.oh.lecture@gmail.com>
2023-09-18 18:02:49 +0900

    update comments

--- lab06_class/src/com/itwill/class05/Account.java
+++ lab06_class/src/com/itwill/class05/Account.java
@@ -12,25 +12,26 @@ public class Account {
     
     // method
     /**
-     * deposit(입금).
+     * deposit(입금). amount를 잔고에 더해줌.
      * 
      * @param amount 입금액.
      * @return 입금 후 잔액.
      */
     
     /**
-     * withdraw(출금).
+     * withdraw(출금). amount를 잔고에서 뺌.
      * 
      * @param amount 출금액.
      * @return 출금 후 잔액.
      */
     
     /**
-     * transfer(이체).
+     * transfer(이체). 내 계좌에서는 잔액에 amount를 더해주고, 
+     * 상대방 계좌에서는 잔액에서 amount를 빼줌.
      * 
      * @param to 이체할 계좌(Account 타입).
      * @param amount 이체할 금액.
-     * @return true. 
+     * @return true.
      */
     
     /**
