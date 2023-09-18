89c13e1822de98f6b7b64312a8cb7d1796b47fd1
JakeOh <jake.oh.lecture@gmail.com>
2023-09-18 16:22:53 +0900

    field, constructor, method

--- lab06_class/src/com/itwill/class04/AppMain04.java
+++ lab06_class/src/com/itwill/class04/AppMain04.java
@@ -12,6 +12,7 @@ public class AppMain04 {
 
         // Score 객체를 아규먼트를 갖는 생성자를 사용해서 생성.
         Score score2 = new Score(100, 50, 70);
+        System.out.println("score2 = " + score2);
         System.out.println("score2 국어 = " + score2.korean);
         System.out.println("score2 총점 = " + score2.getTotal());
         System.out.println("score2 평균 = " + score2.getMean());
@@ -23,6 +24,18 @@ public class AppMain04 {
         System.out.println("stu1 이름 = " + stu1.studentName);
         System.out.println("stu1 점수 = " + stu1.score);
         
+        stu1.studentName = "홍길동";
+        System.out.println("stu1 이름 = " + stu1.studentName);
+        
+        stu1.score = score1;
+        System.out.println("stu1 국어 = " + stu1.score.korean);
+        
+        // argument 3개를 갖는 생성자를 사용해서 Student 타입 객체 생성.
+        Student stu2 = new Student(100, "철수", score2);
+        System.out.println("stu2 = " + stu2);
+        
+        Student stu3 = new Student(101, "오쌤", new Score(50, 50, 70));
+        System.out.println("stu3 = " + stu3);
     }
 
 }
--- lab06_class/src/com/itwill/class04/Student.java
+++ lab06_class/src/com/itwill/class04/Student.java
@@ -7,7 +7,13 @@ public class Student {
     Score score; // 학생 수강(국어/영어/수학) 과목 점수
 
     // 생성자: 기본 생성자, 아규먼트를 갖는 생성자
+    public Student() {}
     
+    public Student(int studentNo, String studentName, Score score) {
+        this.studentNo = studentNo;
+        this.studentName = studentName;
+        this.score = score;
+    }
     
     // 메서드: 학생의 정보를 출력하는 메서드.
     // 번호, 이름, 국어 점수, 영어 점수, 수학 점수, 총점, 평균을 출력.
