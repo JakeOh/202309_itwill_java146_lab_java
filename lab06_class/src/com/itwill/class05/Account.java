59687142a38f79b32f8d65d07b2d86752375fb89
JakeOh <jake.oh.lecture@gmail.com>
2023-09-19 10:24:58 +0900

    account exercise

--- lab06_class/src/com/itwill/class05/Account.java
+++ lab06_class/src/com/itwill/class05/Account.java
@@ -7,8 +7,14 @@ package com.itwill.class05;
  */
 public class Account {
     // field
+    int accountNo;
+    int balance;
     
     // constructor - argument 2개를 갖는 생성자.
+    public Account(int accountNo, int balance) {
+        this.accountNo = accountNo;
+        this.balance = balance;
+    }
     
     // method
     /**
@@ -17,6 +23,15 @@ public class Account {
      * @param amount 입금액.
      * @return 입금 후 잔액.
      */
+    public int deposit(int amount) {
+        if (amount > 0) {
+            this.balance += amount;
+        } else {
+            System.out.println("입금액은 0보다 커야 합니다...");
+        }
+        
+        return this.balance;
+    }
     
     /**
      * withdraw(출금). amount를 잔고에서 뺌.
@@ -24,6 +39,19 @@ public class Account {
      * @param amount 출금액.
      * @return 출금 후 잔액.
      */
+    public int withdraw(int amount) {
+        if (amount > 0) {
+            if (amount <= this.balance) {
+                this.balance -= amount;
+            } else {
+                System.out.println("잔액이 부족합니다...");
+            }
+        } else {
+            System.out.println("출금액은 0보다 커야 합니다.");
+        }
+        
+        return this.balance;
+    }
     
     /**
      * transfer(이체). 내 계좌에서는 잔액에 amount를 더해주고, 
@@ -38,5 +66,11 @@ public class Account {
      * info(정보).
      * 계좌 번호, 현재 잔액을 출력.
      */
+    public void info() {
+        System.out.println("--- 계좌 정보 ---");
+        System.out.println("계좌번호: " + this.accountNo);
+        System.out.println("잔고: " + this.balance);
+        System.out.println("-----------------");
+    }
 
 }
--- lab06_class/src/com/itwill/class05/AppMain05.java
+++ lab06_class/src/com/itwill/class05/AppMain05.java
@@ -3,8 +3,27 @@ package com.itwill.class05;
 public class AppMain05 {
 
     public static void main(String[] args) {
-        // TODO: Account 타입 객체 생성를 생성하고 메서드들을 테스트.
+        // Account 타입 객체 생성를 생성하고 메서드들을 테스트.
 
+        Account account1 = new Account(123456, 10_000);
+        System.out.println(account1);
+        account1.info();
+        
+        Account account2 = new Account(123789, 1_000);
+        System.out.println(account2);
+        account2.info();
+        
+        account1.deposit(2_000);
+        account1.info();
+        
+        account1.deposit(-100);
+        account1.info();
+        
+        account1.withdraw(3_000);
+        account1.info();
+        
+        account1.withdraw(10_000);
+        
     }
 
 }
