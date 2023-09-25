b5b28d599229bd505203d6ca5637cb1644afad14
JakeOh <jake.oh.lecture@gmail.com>
2023-09-25 10:25:55 +0900

    toString, equals, hashCode

--- lab07_inheritance/src/com/itwill/inheritance06/InheritanceMain06.java
+++ lab07_inheritance/src/com/itwill/inheritance06/InheritanceMain06.java
@@ -14,7 +14,34 @@ public class InheritanceMain06 {
         
         System.out.println(p1.hashCode());
         System.out.println(p2.hashCode());
+        
+        System.out.println("----------");
+        
+        User user1 = new User();
+        System.out.println(user1);
+        
+        User user2 = new User("admin", "0000");
+        System.out.println(user2);
+        
+        User user3 = new User("admin", "1234");
+        System.out.println(user3);
+        
+        // 비교 연산자 결과
+        System.out.println("== 결과");
+        System.out.println(user1 == user2); //-> false
+        System.out.println(user2 == user3); //-> false
+        
+        // equals 메서드 결과
+        System.out.println("equals 메서드 결과");
+        System.out.println(user1.equals(user2)); //-> false
+        System.out.println(user2.equals(user3)); //-> true
 
+        // hashCode 결과
+        System.out.println("hashCode 메서드 결과");
+        System.out.println(user1.hashCode());
+        System.out.println(user2.hashCode());
+        System.out.println(user3.hashCode());
+        
     }
 
 }
--- lab07_inheritance/src/com/itwill/inheritance06/User.java
+++ lab07_inheritance/src/com/itwill/inheritance06/User.java
@@ -5,12 +5,43 @@ public class User {
     private String id;
     private String password;
     
-    // TODO: 생성자 - 기본 생성자, 아규먼트 2개인 생성자
+    // 생성자 - 기본 생성자, 아규먼트 2개인 생성자
+    public User() {}
     
-    // TODO: toString 재정의
+    public User(String id, String password) {
+        this.id = id;
+        this.password = password;
+    }
     
-    // TODO: equals 재정의 - id가 같으면 true, 그렇지 않으면 false.
+    // toString 재정의
+    @Override
+    public String toString() {
+        return "User(id=" + this.id + ", password=" + this.password + ")";
+    }
     
-    // TODO: hashCode 재정의
+    // equals 재정의 - id가 같으면 true, 그렇지 않으면 false.
+    @Override
+    public boolean equals(Object obj) {
+        boolean result = false;
+        
+        if (obj instanceof User) {
+            User other = (User) obj; // 타입 변환(casting)
+            if (this.id != null) { // null이 아닌 경우에만 메서드 호출 가능!
+                result = this.id.equals(other.id); // String 비교할 때 equals 메서드 사용!
+            }
+        }
+        
+        return result;
+    }
+    
+    // hashCode 재정의
+    @Override
+    public int hashCode() {
+        if (this.id != null) {
+            return this.id.hashCode();
+        } else {
+            return 0;
+        }
+    }
 
 }
