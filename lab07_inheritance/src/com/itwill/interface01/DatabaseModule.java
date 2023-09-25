f39af88bf6cf40d311ffa988f90a303d5bae2f90
JakeOh <jake.oh.lecture@gmail.com>
2023-09-25 14:22:56 +0900

    interface

---
+++ lab07_inheritance/src/com/itwill/interface01/DatabaseModule.java
@@ -0,0 +1,22 @@
+package com.itwill.interface01;
+
+public interface DatabaseModule {
+    // 필드
+    int DB_VERSION = 1; // public static final 생략.
+
+    /**
+     * 데이터베이스의 테이블에서 자료들을 읽어서 정수를 리턴하는 기능.
+     * 
+     * @return 검색된 자료 개수.
+     */
+    int select(); // public abstract 생략.
+    
+    /**
+     * 데이터베이스의 테이블에 문자열을 저장하는 기능.
+     * 
+     * @param value. 테이블에 저장할 문자열.
+     * @return 저장 성공하면 1, 실패하면 0.
+     */
+    int insert(String value);
+    
+}
---
+++ lab07_inheritance/src/com/itwill/interface01/InterfaceMain01.java
@@ -0,0 +1,24 @@
+package com.itwill.interface01;
+
+/*
+ * 인터페이스(interface):
+ * 사용 목적: 팀/회사 간의 분업/협업을 하기 위해서 메서드들의 signature를 약속하기 위한 규약.
+ * 인터페이스가 가질 수 있는 멤버들:
+ *   (1) public static final 필드. "public static final" 수식어는 보통 생략.
+ *   (2) public abstract 메서드. "public abstract" 수식어는 보통 생략.
+ * 인터페이스를 작성할 때는 "interface" 키워드로 선언.
+ * 인터페이스는 객체를 생성할 수 없고, 인터페이스를 "구현"하는 클래스를 작성해서 객체를 생성.
+ *   class 클래스이름 implements 인터페이스 { ... }
+ * 클래스는 단일 상속만 가능하지만, 인터페이스 구현은 개수 제한이 없음.
+ * 인터페이스는 상위 인터페이스를 상속(확장)할 수도 있음.
+ *   interface 하위인터페이스 extends 상위인터페이스 { ... }
+ */
+
+public class InterfaceMain01 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
