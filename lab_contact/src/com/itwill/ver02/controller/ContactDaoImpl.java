237478f89a19b1ad28557e8ada8ae295d38fbd1c
JakeOh <jake.oh.lecture@gmail.com>
2023-09-26 17:34:28 +0900

    Contact v0.2

--- lab_contact/src/com/itwill/ver02/controller/ContactDaoImpl.java
+++ lab_contact/src/com/itwill/ver02/controller/ContactDaoImpl.java
@@ -1,5 +1,47 @@
 package com.itwill.ver02.controller;
 
+import com.itwill.ver02.model.Contact;
+
 public class ContactDaoImpl implements ContactDao {
+    
+    private static ContactDaoImpl instance = null;
+    
+    private ContactDaoImpl() {}
+    
+    public static ContactDaoImpl getInstance() {
+        if (instance == null) {
+            instance = new ContactDaoImpl();
+        }
+        
+        return null;
+    }
+
+    public static final int MAX_LENGTH = 2;
+    private Contact[] contacts = new Contact[MAX_LENGTH];
+    private int count = 0;
+    
+    @Override
+    public Contact[] read() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int create(Contact contact) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Contact read(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int update(int index, Contact contact) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
 }
