b025b4600fdd9694668e7d1a95288e2540f5cd17
JakeOh <jake.oh.lecture@gmail.com>
2023-10-04 16:34:20 +0900

    String class methods

--- lab10_api/src/com/itwill/string/StringEx.java
+++ lab10_api/src/com/itwill/string/StringEx.java
@@ -9,6 +9,18 @@ public class StringEx {
         String ssn = "990904-1234567";
         System.out.println(ssn.charAt(7));
 
+        // ex2. 아래의 문자열 배열에서 5글자 이상인 문자열들만 출력.
+        // 결과: JavaScript Servlet Spring
+        String[] array = {"Java", "SQL", "JavaScript", "HTML", "CSS", "Servlet", "Spring"};
+        
+        // ex3. 아래의 문자열 배열에서 "홍길동" 문자열이 처음 등장하는 인덱스를 출력.
+        // 만약에 "홍길동"이 배열에 없는 경우에는 -1을 출력.
+        // 결과: 2
+        String[] names = {"오쌤", "John", "홍길동", "Gildong", "홍길동"};
+        
+        // ex4. 아래의 문자열 배열에서 대소문자 구별없이 "est"를 포함하는 문자열들을 출력.
+        // 결과: TEST test TeSt tEsT
+        String[] tests = {"TEST", "test", "TeSt", "tEsT", "테스트"};
     }
 
 }
