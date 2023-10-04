78bd19dc642f4c9a0f9150e7e22c51906411822e
JakeOh <jake.oh.lecture@gmail.com>
2023-10-04 14:17:00 +0900

    try-catch

--- lab09_exception/src/com/itwill/exception02/ExceptionMain02.java
+++ lab09_exception/src/com/itwill/exception02/ExceptionMain02.java
@@ -16,6 +16,11 @@ import java.util.Scanner;
  * 예외가 발생하지 않을 경우: (1) --> (3)
  * 예외가 발생하는 경우: (1) --> 예외 발생 --> (2) --> (3)
  * 
+ * try-catch 문장에서 catch 블록은 1개 이상이 있어야 함.
+ * finally 블록은 선택.
+ * catch 블록은 처리하려고 하는 예외 종류에 따라서 여러개를 작성할 수 있음.
+ * catch 블록이 여러개일 때는 하위 타입의 예외들을 먼저 catch하고,
+ * 상위 타입의 예외들을 나중에 catch해야 함.
  */
 
 public class ExceptionMain02 {
@@ -36,6 +41,8 @@ public class ExceptionMain02 {
             System.out.println("y는 0이 될 수 없음.");
         } catch (NumberFormatException e) {
             System.out.println("x와 y는 정수로만 입력해야 함.");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         } finally {
             System.out.println("Finally...");
         }
---
+++ lab09_exception/src/com/itwill/exception03/ExceptionMain03.java
@@ -0,0 +1,33 @@
+package com.itwill.exception03;
+
+import java.util.Scanner;
+
+public class ExceptionMain03 {
+    
+    private Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // ExceptionMain03 타입의 객체 생성
+        ExceptionMain03 app = new ExceptionMain03();
+        
+        int x = app.inputInteger();
+        System.out.println("x = " + x);
+
+    }
+    
+    public int inputInteger() {
+        int result = 0;
+        while (true) {
+            try {
+                System.out.print("정수 입력> ");
+                result = Integer.parseInt(sc.nextLine());
+                break; // 무한 반복문 종료
+            } catch (NumberFormatException e) {
+                System.out.println("입력한 값은 정수가 아닙니다.");
+            }
+        }
+        
+        return result;
+    }
+
+}
