28bc7dad5a3aaa0ea98ccfb3225d324ab9a5084d
JakeOh <jake.oh.lecture@gmail.com>
2023-10-04 17:19:56 +0900

    String class methods

--- lab10_api/src/com/itwill/string/StringEx.java
+++ lab10_api/src/com/itwill/string/StringEx.java
@@ -12,17 +12,32 @@ public class StringEx {
         // ex2. 아래의 문자열 배열에서 5글자 이상인 문자열들만 출력.
         // 결과: JavaScript Servlet Spring
         String[] array = {"Java", "SQL", "JavaScript", "HTML", "CSS", "Servlet", "Spring"};
+        for (String s : array) {
+            if (s.length() >= 5) {
+                System.out.print(s + " ");
+            }
+        }
+        System.out.println();
         
         // ex3. 아래의 문자열 배열에서 "홍길동" 문자열이 처음 등장하는 인덱스를 출력.
         // 만약에 "홍길동"이 배열에 없는 경우에는 -1을 출력.
         // 결과: 2
         String[] names = {"오쌤", "John", "홍길동", "Gildong", "홍길동"};
+        int index = -1;
+        for (int i = 0; i < names.length; i++) {
+            if (names[i].equals("홍길동")) {
+                index = i;
+                break;
+            }
+        }
+        System.out.println(index);
         
         // ex4. 아래의 문자열 배열에서 대소문자 구별없이 "est"를 포함하는 문자열들을 출력.
         // 결과: TEST test TeSt tEsT
         String[] tests = {"TEST", "test", "TeSt", "tEsT", "테스트"};
         
-        // ex5. 아래의 "YYYY-MM-DD" 형식의 날짜 문자열에서 년/월/일 정보를 int 타입 변수에 저장하고 출력.
+        
+        // ex5. 아래의 "YYYY-MM-DD" 형식의 문자열에서 년/월/일 정보를 int 타입 변수에 저장하고 출력.
         // 결과: year=2023, month=10, day=4
         String date = "2023-10-04";
     }
