b9fd2b7b87d25069381daee014e49d20e42b8072
JakeOh <jake.oh.lecture@gmail.com>
2023-10-04 16:41:59 +0900

    String class methods

--- lab10_api/src/com/itwill/string/StringEx.java
+++ lab10_api/src/com/itwill/string/StringEx.java
@@ -21,6 +21,10 @@ public class StringEx {
         // ex4. 아래의 문자열 배열에서 대소문자 구별없이 "est"를 포함하는 문자열들을 출력.
         // 결과: TEST test TeSt tEsT
         String[] tests = {"TEST", "test", "TeSt", "tEsT", "테스트"};
+        
+        // ex5. 아래의 "YYYY-MM-DD" 형식의 날짜 문자열에서 년/월/일 정보를 int 타입 변수에 저장하고 출력.
+        // 결과: year=2023, month=10, day=4
+        String date = "2023-10-04";
     }
 
 }
