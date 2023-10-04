7672f7563b4ab6da71dbb206588895ad443d9ae5
JakeOh <jake.oh.lecture@gmail.com>
2023-10-04 16:06:48 +0900

    Exception

--- lab09_exception/src/com/itwill/exception06/ExceptionMain06.java
+++ lab09_exception/src/com/itwill/exception06/ExceptionMain06.java
@@ -1,5 +1,18 @@
 package com.itwill.exception06;
 
+/*
+ * 예외 클래스들의 상속 관계:
+ * Object
+ * |__ Throwable
+ *     |__ Exception
+ *         |__ RuntimeException, IOException, ...
+ *             |__ ArtithmeticException, NullPointerException, NumberFormatException, ...
+ * throws 선언문이 있는 메서드들 중에서 RuntimeException과 그 하위 예외 클래스들을 던지는(throw)
+ * 메서드들은 try-catch를 사용하지 않아도 컴파일 가능.
+ * RuntimeException이 아닌 예외를 던진다고 선언한 메서드들은 반드시 try-catch를 사용해야 함.
+ * 
+ */
+
 public class ExceptionMain06 {
 
     public static void main(String[] args) {
