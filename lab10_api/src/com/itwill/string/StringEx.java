32335472e5b03005efc10b250a2dd9993579abf6
JakeOh <jake.oh.lecture@gmail.com>
2023-10-04 16:22:20 +0900

    String class methods

---
+++ lab10_api/src/com/itwill/string/StringEx.java
@@ -0,0 +1,14 @@
+package com.itwill.string;
+
+public class StringEx {
+
+    public static void main(String[] args) {
+        // String 클래스의 메서드 사용법
+        
+        // ex1. 주민번호 문자열에서 성별을 표시하는 위치의 문자를 출력.
+        String ssn = "990904-1234567";
+        System.out.println(ssn.charAt(7));
+
+    }
+
+}
