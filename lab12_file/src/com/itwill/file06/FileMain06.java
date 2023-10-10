8f90ab4bb1fbf48caa05260abfd9358d0ce9e247
JakeOh <jake.oh.lecture@gmail.com>
2023-10-10 17:29:37 +0900

    modified comments

--- lab12_file/src/com/itwill/file06/FileMain06.java
+++ lab12_file/src/com/itwill/file06/FileMain06.java
@@ -5,9 +5,11 @@ public class FileMain06 {
     public static void main(String[] args) {
         // 1) com.itwill.file05.Product 타입 객체 1,000,000개를 저장하는 ArrayList를 파일에 write:
         // FileOutputStream, BufferedOutputStream, ObjectOutputStream 사용.
+        // ArrayList를 파일에 쓰는 시간을 측정하고 출력.
         
         // 2) 1)에서 작성된 파일에서 ArrayList를 읽고, 내용 확인:
         // FileInputStream, BufferedInputStream, ObjectInputStream 사용.
+        // ArrayList를 파일에서 읽는 시간을 측정하고 출력.
 
     }
 
