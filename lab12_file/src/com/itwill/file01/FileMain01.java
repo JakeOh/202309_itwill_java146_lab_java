dfee7547fdfe04c078bbd93e22167a917da98f50
JakeOh <jake.oh.lecture@gmail.com>
2023-10-10 12:22:39 +0900

    file input/output stream

--- lab12_file/src/com/itwill/file01/FileMain01.java
+++ lab12_file/src/com/itwill/file01/FileMain01.java
@@ -1,5 +1,8 @@
 package com.itwill.file01;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 /*
  * 입/출력 스트림(Input/Output Stream): 프로그램에서 값을 입력받거나 출력하는 통로.
  *   (예) System.in: 입력 스트림 객체, System.out: 출력 스트림 객체.
@@ -25,7 +28,42 @@ package com.itwill.file01;
 public class FileMain01 {
     
     public static void main(String[] args) {
+        String fileName = "data/hello.txt"; // 읽을 파일 경로, 이름.
+        String copyFile = "data/hello_copy.txt"; // 복사할 파일 경로, 이름.
         
+        FileInputStream in = null;
+        FileOutputStream out = null;
+        try {
+            // 1. FIS 객체 생성.
+            in = new FileInputStream(fileName);
+            
+            // 1. FOS 객체 생성.
+            out = new FileOutputStream(copyFile);
+            
+            while (true) {
+                // 2. read 메서드 사용.
+                int read = in.read();
+                if (read == -1) { // 파일 끝(EOF, end-of-file)에 도달하면
+                    break;
+                }
+                // System.out.println((char) read);
+                
+                // 2. FOS의 write 메서드 사용.
+                out.write(read);
+            }
+            
+            System.out.println("파일 복사 성공");
+            
+        } catch (Exception e) {
+            e.printStackTrace(); // 예외 이름, 메시지, 발생 위치를 출력.
+        } finally {
+            try {
+                in.close(); // 3. FIS 객체를 닫음.
+                out.close(); // 3. FOS 객체를 닫음.
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
     
 }
