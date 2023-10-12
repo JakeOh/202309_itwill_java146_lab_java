213899888c62ba48a19c0dc6e894ec7a5dcf45de
JakeOh <jake.oh.lecture@gmail.com>
2023-10-12 15:24:29 +0900

    builder(factory) pattern

--- lab13_inner/src/com/itwill/inner02/Book.java
+++ lab13_inner/src/com/itwill/inner02/Book.java
@@ -25,5 +25,49 @@ public class Book {
 //        this.author = author;
         this(title, author, null);
     }
+    
+    // 생성자 오버로딩(overloading):
+    // 파라미터(개수, 타입)가 다른 생성자들을 여러개 작성.
+    // Book(String title, String author) 생성자가 있는 경우,
+    // Book(String title, String publisher), Book(String author, String publisher) 생성자는 오버로딩할 수 없음.
+    
+    @Override
+    public String toString() {
+        return String.format("Book{제목=%s, 저자=%s, 출판사=%s}", 
+                title, author, publisher);
+    }
 
-}
+    //----- Builder(Factory) 디자인 패턴 -----//
+    public static BookBuilder builder() {
+        return new BookBuilder();
+    }
+    
+    public static class BookBuilder {
+        private String title;
+        private String author;
+        private String publisher;
+        
+        private BookBuilder() {}
+        
+        public BookBuilder title(String title) {
+            this.title = title;
+            return this;
+        }
+        
+        public BookBuilder author(String author) {
+            this.author = author;
+            return this;
+        }
+        
+        public BookBuilder publisher(String publisher) {
+            this.publisher = publisher;
+            return this;
+        }
+        
+        public Book build() {
+            return new Book(title, author, publisher);
+        }
+        
+    } // class BookBuilder
+    
+} // class Book
--- lab13_inner/src/com/itwill/inner02/InnterMain02.java
+++ lab13_inner/src/com/itwill/inner02/InnterMain02.java
@@ -3,8 +3,30 @@ package com.itwill.inner02;
 public class InnterMain02 {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        // Book 타입 객체 생성
+        Book book1 = new Book("일론 머스크", "월터 아이작슨", "21세기북스");
+        System.out.println(book1);
+        
+        Book book2 = new Book("일론 머스크", "월터 아이작슨");
+        System.out.println(book2);
+        
+        Book book3 = new Book("월터 아이작슨", "일론 머스크");
+        System.out.println(book3);
 
+        // Builder 패턴을 이용한 객체 생성
+        Book book4 = Book.builder().build();
+        System.out.println(book4);
+        
+        Book book5 = Book.builder().author("월터 아이작슨").build();
+        System.out.println(book5);
+        
+        Book book6 = Book.builder()
+                .publisher("21세기북스")
+                .title("일론 머스크")
+                .author("월터 아이작슨")
+                .build();
+        System.out.println(book6);
+        
     }
 
 }
