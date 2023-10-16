319fcb941fc21e89f89dbce6ccb744561a36b75b
JakeOh <jake.oh.lecture@gmail.com>
2023-10-16 15:07:33 +0900

    swing, window builder

--- lab15_swing/src/com/itwill/swing01/AppMain01.java
+++ lab15_swing/src/com/itwill/swing01/AppMain01.java
@@ -3,6 +3,10 @@ package com.itwill.swing01;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.Font;
+import javax.swing.SwingConstants;
 
 public class AppMain01 {
 
@@ -17,7 +21,7 @@ public class AppMain01 {
             public void run() {
                 try {
                     AppMain01 window = new AppMain01();
-                    window.frame.setVisible(true);
+                    window.frame.setVisible(true); // JFrame을 화면에 보여줌.
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -36,9 +40,15 @@ public class AppMain01 {
      * Initialize the contents of the frame.
      */
     private void initialize() {
-        frame = new JFrame();
-        frame.setBounds(100, 100, 450, 300);
+        frame = new JFrame(); // JFrame 객체 생성, 필드 초기화.
+        frame.setBounds(100, 100, 600, 400); // JFrame의 시작 좌표(x, y)와 크기(가로, 세로) 설정
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //-> JFrame 우상단에 있는 닫기 버튼(x)의 기능(동작)을 설정 - 프로그램 종료
+        
+        JLabel lblMessage = new JLabel("Hello, Swing!");
+        lblMessage.setHorizontalAlignment(SwingConstants.CENTER);
+        lblMessage.setFont(new Font("D2Coding", Font.BOLD | Font.ITALIC, 24));
+        frame.getContentPane().add(lblMessage, BorderLayout.CENTER);
     }
 
 }
