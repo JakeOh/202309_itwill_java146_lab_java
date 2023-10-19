1a3502de2ff5fa96a6926ea657defeb5a9912588
JakeOh <jake.oh.lecture@gmail.com>
2023-10-19 16:25:51 +0900

    select, order by, distinct

--- lab_sql/sql02_select.sql
+++ lab_sql/sql02_select.sql
@@ -28,4 +28,45 @@ select deptno || '-' || dname as "부서 정보"
 from dept;
 
 -- 부서 테이블을 검색해서 '... 부서는 ...에 있습니다.' 형식으로 결과 출력:
+select
+    dname || ' 부서는' || loc || '에 있습니다.' as "부서 정보"
+from dept;
 
+-- 검색 결과를 정렬해서 출력하기:
+-- select 컬럼 이름, ... from 테이블 order by 컬럼 [asc/desc];
+-- asc: 오름차순(ascending order) 정렬. 기본값. asc는 생략 가능.
+-- desc: 내림차순(descending order) 정렬.
+
+-- 부서 테이블의 모든 컬럼을 검색, 부서 번호 내림차순으로 출력:
+select * from dept order by deptno desc;
+
+-- 부서 테이블의 모든 컬럼을 검색, 부서 이름 오름차순으로 출력:
+select * from dept order by dname;
+
+-- 직원 테이블에서 사번, 이름, 급여를 검색, 사번 오름차순으로 출력:
+select empno, ename, sal from emp order by empno;
+
+-- 직원 테이블에서 사번, 이름, 급여를 검색, 급여 내림차순으로 출력:
+select empno, ename, sal from emp order by sal desc;
+
+-- 직원 테이블에서 부서번호, 사번, 이름을 검색
+-- 정렬 순서: (1) 부서번호 오름차순, (2) 사번 오름차순
+select deptno, empno, ename
+from emp
+order by deptno, empno;
+
+-- 직원 테이블에서 부서번호, 이름, 급여를 검색
+-- 정렬 순서: (1) 부서번호 오름차순, (2) 급여 내림차순
+select deptno, ename, sal
+from emp
+order by deptno, sal desc;
+
+-- 중복되지 않는 결과만 출력하기:
+select job from emp;
+select distinct job from emp; -- 중복되지 않는 업무이름들을 검색
+-- (중복되지 않는) 업무 이름을 오름차순 정렬 출력:
+select distinct job from emp order by job;
+
+-- 중복되지 않는 부서번호, 직무를 출력, 부서번호 오름차순 정렬:
+select distinct deptno, job from emp
+order by deptno;
