adc63ecdff9e9748f70adacbe1bdfc0159472e4e
JakeOh <jake.oh.lecture@gmail.com>
2023-10-20 17:18:48 +0900

    subquery

--- lab_sql/sql05_subquery.sql
+++ lab_sql/sql05_subquery.sql
@@ -51,8 +51,46 @@ where sal * 12 + nvl(comm, 0) > (
 order by ANNUAL_SAL desc;
 
 -- SCOTT과 같은 급여를  받는 직원들의 이름과 급여를 검색.
+select ename, sal
+from emp
+where sal = (
+    select sal from emp where ename = 'SCOTT'
+);
+
 -- 위 결과에서 SCOTT은 제외하고 검색.
+select ename, sal
+from emp
+where ename != 'SCOTT'
+    and
+    sal = (
+        select sal from emp where ename = 'SCOTT'
+    );
+
 -- ALLEN보다 늦게 입사한 직원들의 이름, 입사날짜를 최근입사일부터 출력.
+select ename, hiredate
+from emp
+where hiredate > (
+    select hiredate from emp where ename = 'ALLEN'
+)
+order by hiredate desc;
+
 -- 매니저가 KING인 직원들의 사번, 이름, 매니저 사번을 검색.
+select empno, ename, mgr
+from emp
+where mgr = (
+    select empno from emp where ename = 'KING'
+);
+
 -- ACCOUNTING 부서에 일하는 직원들의 이름, 급여, 부서번호를 검색.
+select ename, sal, deptno
+from emp
+where deptno = (
+    select deptno from dept where dname = 'ACCOUNTING'
+);
+
 -- CHICAGO에서 근무하는 직원들의 이름, 급여, 부서 번호를 검색.
+select ename, sal, deptno
+from emp
+where deptno = (
+    select deptno from dept where loc = 'CHICAGO'
+);
